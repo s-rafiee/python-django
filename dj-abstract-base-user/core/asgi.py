5611684dec5d8299de7de9c9fde9dbe255fc7b56
Saman Rafiee <saman.rafiee.rs@google.com>
2024-01-26 22:42:09 +0330

    Create custom AbstractBaseUser

--- dj-abstract-base-user/core/asgi.py
+++ dj-abstract-base-user/core/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for dj-abstract-base-user project.
+ASGI config for core project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'dj-abstract-base-user.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
 
 application = get_asgi_application()
